eee64c786b2b61d257cdf7e9bc4e6b6015025f8f
Tim Ord <tim.ord93@gmail.com>
2015-12-21 14:45:44 +0000

    Update js.js

--- js.js
+++ js.js
@@ -1,12 +1,16 @@
 jQuery(function($) {
   
   var names = $('#textarea').val();
-  var list = new Array();
-  list = names.split(',');
+  //var list = new Array();
+  //list = names.split(',');
+  
+  var randomPicker = names[Math.floor(Math.random() * names.length)];
+  
+  var selected = randomPicker[0];
   
   
 
   $('button').click(function(){
-    console.log(list);
+    alert(selected);
   });
 });
