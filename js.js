e5919e4ee8dc9f40808ba805f12aa3553c7d894d
Tim Ord <tim.ord93@gmail.com>
2015-12-21 14:28:29 +0000

    Create js.js

---
+++ js.js
@@ -0,0 +1,12 @@
+jQuery function($) {
+  
+  var names = $('textarea').val();
+  var list = array();
+  list = names.split(',');
+  
+  
+
+  $('button').click(function(){
+    alert(list);
+  });
+}
