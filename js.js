2fde7c3757ddc972003653d1575e6a2cb2fe27e9
Tim Ord <tim.ord93@gmail.com>
2016-02-24 20:51:05 +0000

    Update js.js

--- js.js
+++ js.js
@@ -1,14 +1,38 @@
 jQuery(function($){
+
+$('button.btn-add').click(function(e){
+	e.preventDefault();
+	$('.name-boxes').append('<div class="form-inline"><input type="text" class="form-control name-input input-lg" placeholder="Name"> <span class="glyphicon glyphicon-remove"></span></div>');
+});
+
+$('button.btn-clear').click(function(e){
+	e.preventDefault();
+	$('input.name-input, input.name-input + span').remove();
+	$('p.answer').empty().removeClass('bg-success bg-danger');
+	$('.name-boxes').append('<div class="form-inline"><input type="text" class="form-control name-input input-lg" placeholder="Name"> <span class="glyphicon glyphicon-remove"></span></div>');
+});	
+
+$('span.glyphicon-remove').live('click', function(){
+	$(this).prev('input').remove();
+	$(this).remove();
+});
   
-  $('button').click(function(){
-  var names = $('#textarea').val();
-  var list = new Array();
-  list = names.split(',');
+  $('button.btn-tea').live('click', function(){
+
+  var list = [];
+  $("input.name-input").each(function(){
+    list.push($(this).val());
+  });
     
   var randomPicker = list[Math.floor(Math.random() * list.length)];
   var selected = randomPicker; 
 
-    $('p.bg-success').html(selected);
-    
+if( selected !== '' ) {
+    $('p').removeClass('bg-danger').addClass('bg-success').html('It\'s ' + selected + ', off you trot!');
+} else {
+	$('p').removeClass('bg-success').addClass('bg-danger').html("You didn't enter any names, you badass! <span class='glyphicon glyphicon-thumbs-down'></span>");
+}
+
   });
+
 });
